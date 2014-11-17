2d13bb7712174d96c0836f2f465c5f023db48eaa
dacuevas <dcuevas08@gmail.com>
2014-11-17 11:19:22 -0800

    - Fixed bug in calling logistic function. - Code clean up.

--- py/GrowthCurve.py
+++ py/GrowthCurve.py
@@ -4,7 +4,7 @@
 #
 # Author: Daniel A Cuevas
 # Created on 21 Nov. 2013
-# Updated on 26 Aug. 2014
+# Updated on 17 Nov. 2014
 
 
 import pylab as py
@@ -15,24 +15,20 @@ import sys
 class GrowthCurve:
     '''Bacteria growth curve class'''
     def __init__(self, data, time):
-        # data format: multidimensional numpy array
-        #              Each inner array is an array of OD values
-        #              ordered by time.
-        #              This is important for determining the median
+        # data format:  numpy array if OD values
 
-        self.dataReps = data  # OD data values (replicates implied)
-        #self.dataMed = py.median(self.dataReps, axis=0)
+        self.rawcurve = data  # OD data values (replicates implied)
         self.time = time  # time values
-        self.y0 = self.dataReps[1]
+        self.y0 = self.rawcurve[1]
         self.asymptote = self.__calcAsymptote()
         self.maxGrowthRate, self.mgrTime = self.__calcMGR()
         self.asymptote, self.maxGrowthRate, self.lag = self.__calcParameters(
-            (self.asymptote, self.maxGrowthRate, 0.5), self.time, self.dataReps)
+            (self.asymptote, self.maxGrowthRate, 0.5), self.time, self.rawcurve)
 
-        self.dataLogistic = logistic(self.y0, self.time,
+        self.dataLogistic = logistic(self.time, self.y0,
                                      self.asymptote, self.maxGrowthRate, self.lag)
         self.growthLevel = self.__calcGrowth()
-        self.sse = sum((self.dataLogistic - self.dataReps) ** 2)
+        self.sse = sum((self.dataLogistic - self.rawcurve) ** 2)
 
     def __calcParameters(self, y0, t, raw):
         '''Perform curve-fitting optimization to obtain parameters'''
@@ -43,7 +39,7 @@ class GrowthCurve:
                                                 (0, None)))
         except RuntimeError as e:
             print(e)
-            print(self.dataReps)
+            print(self.rawcurve)
             sys.exit(1)
 
         return results.x
@@ -53,18 +49,18 @@ class GrowthCurve:
         return py.sum((logistic(t, self.y0, a, mgr, l) - y) ** 2)
 
     def __calcAsymptote(self):
-        '''Obtain the value of the highest OD reading'''
+        '''Obtain the initial guess of the highest OD reading'''
         # Calculate asymptote using a sliding window of 3 data points
         stop = len(self.time) - 3
         maxA = -1
         for idx in range(1, stop):
-            av = py.mean(self.dataReps[idx:idx + 3])
+            av = py.mean(self.rawcurve[idx:idx + 3])
             if av > maxA:
                 maxA = av
         return maxA
 
     def __calcMGR(self):
-        '''Obtain the value of the max growth'''
+        '''Obtain the initial guess of the max growth'''
         # Calculate max growth rate using a sliding window of 4 data points
         stop = len(self.time) - 4
         maxGR = 0
@@ -73,7 +69,7 @@ class GrowthCurve:
 
             # Growth rate calculation:
             # (log(i+3) - log(i)) / (time(i+3) - time(i))
-            gr = ((py.log(self.dataReps[idx + 3]) - py.log(self.dataReps[idx])) /
+            gr = ((py.log(self.rawcurve[idx + 3]) - py.log(self.rawcurve[idx])) /
                   (self.time[idx + 3] - self.time[idx]))
             if idx == 1 or gr > maxGR:
                 maxGR = gr
