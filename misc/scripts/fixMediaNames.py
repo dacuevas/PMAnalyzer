1324000534df7b0397a82f577fb51db06f4bd2a0
dacuevas <dcuevas08@gmail.com>
2016-06-13 14:48:40 -0700

    fixMediaNames.py - Initial commit.

---
+++ misc/scripts/fixMediaNames.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python2.7
+from __future__ import print_function
+import argparse
+import re
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input", help="Input PMAnalyzer file")
+parser.add_argument("plate", help="Plate file")
+parser.add_argument("-n", "--nocolumn", action="store_true",
+                    help="Plate information columns do not exist")
+
+args = parser.parse_args()
+
+plateinfo = {}
+with open(args.plate, "r") as f:
+    for l in f:
+        w, ms, c, conc = l.strip().split("\t")
+        plateinfo[w] = (ms, c)
+
+with open(args.input, "r") as f:
+    header = next(f)
+    header = header.strip()
+    ll = header.split("\t")
+    wellIdx = ll.index("well")
+    if args.nocolumn:
+        # Plate information does not exist yet
+        # Print out new columns
+        header = "\t".join(ll[0:2])
+        header += "\tmainsource\tcompound\tplate\t"
+        header += "\t".join(ll[2:])
+    print(header)
+    for l in f:
+        l = l.strip()
+        ll = l.split("\t")
+        w = ll[wellIdx]
+        if args.nocolumn:
+            # Plate information does not exist yet
+            # Add new columns
+            p = re.search(r"([A-z0-9-_.]+).txt$", args.plate).group(1)
+            line = "\t".join(ll[0:2]) + "\t"
+            line += "\t".join([ms, c, p]) + "\t"
+            line += "\t".join(ll[2:])
+        else:
+            # Plate information already exists
+            # Replace mainsource and compound
+            ms, c = plateinfo[w]
+            line = "\t".join(ll[0:2]) + "\t"
+            line += "\t".join([ms, c]) + "\t"
+            line += "\t".join(ll[4:])
+        print(line)
+
+print("Complete!", file=sys.stderr)
